a47d3c65b322a3861dda1c19e30b04314555c2e3
Paolo Posso <pvictorsys@gmail.com>
2022-12-28 14:24:16 -0300

    read all files

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 
 	k8sparser "github.com/paoloposso/k8sparser/src"
 )
 
 func main() {
-	yamlFileBytes, err := ioutil.ReadFile("./tests/examples/mongo.yaml")
-	if err != nil {
-		panic(err)
-	}
 
-	k8sparser.ParseFile(yamlFileBytes)
+	files, _ := ioutil.ReadDir("./tests/examples")
+
+	fmt.Println(files)
+
+	for _, item := range files {
+		fmt.Println(item.Name())
+		yamlFileBytes, err := ioutil.ReadFile(fmt.Sprintf("./tests/examples/%s", item.Name()))
+
+		if err != nil {
+			panic(err)
+		}
+
+		k8sparser.ParseFile(yamlFileBytes)
+	}
 }
